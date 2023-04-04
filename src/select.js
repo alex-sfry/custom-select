d9281e4a23f1d356ae978735bdcd5539553acb4f
Aleksandr Tochilin <alex8079@gmail.com>
2023-04-04 13:16:05 +0300

    change appearance and location of remove button for single select

--- src/select.js
+++ src/select.js
@@ -3,98 +3,79 @@ import createHTML from './createhtml.js';
 
 export class Select {
     constructor(config) {
-        if (!config.select) {
-            return console.log('provide id of select element');
-        }
-
+        if (!config.select) return console.log('provide id of select element');
         this.select = document.querySelector(`#${config.select}`);
         this.multi = this.select.multiple;
-
-        if (!this.select) {
-            return console.log(`select element with id '${config.select}' not found`);
-        }
+        if (!this.select) return console.log(`select element with id '${config.select}' not found`);
 
         this.createHTML = createHTML.bind(this);
         this.idSelect = `c-select${this.uniqueID()}`;
         this.options = this.select.querySelectorAll('option');
+
         this.createHTML();
         this.createOptionList();
+
         this.firstOptionItem = document.querySelector(`#${this.idSelect} .c-select__dropdown-item:first-of-type`);
         this.selectOptions = document.querySelectorAll(`#${this.idSelect} option`);
+        
         this.setEventListeners(config);
         this.filterOptionItems();
+
+        this.input = document.querySelector(`#${this.idSelect} .c-select__input`);
+        this.selectedDiv = document.querySelector(`#${this.idSelect} .c-select__selected`);
+        this.inputDiv = document.querySelector(`#${this.idSelect} .c-select__input-div`);
+        this.arrowIcon = document.querySelector(`#${this.idSelect} .c-select__arrow-icon`);
+        this.dropdown = document.querySelector(`#${this.idSelect} .c-select__dropdown`);
+        this.SPACEBAR_KEY_CODE = 32;
+        this.ENTER_KEY_CODE = 13;
+        this.UP_KEY_CODE = 38;
+        this.DOWN_KEY_CODE = 40;
+        this.dropdownItems = document.querySelectorAll(`#${this.idSelect} .c-select__dropdown-item`);
     }
 
     uniqueID() {
         return Math.floor(Math.random() * Date.now()).toString();
     }
 
-    setEventListeners(config) {
+    setEventListeners() {
         document.querySelector(`#${this.idSelect} .c-select__option-list`)
-            .addEventListener('click', (e) => this.handleClick(e, this.idSelect, config));
+            .addEventListener('click', (e) => this.handleClick(e));
 
         document.querySelector('body')
-            .addEventListener('click', (e) => this.handleOutsideClick(e, this.idSelect));
-        
+            .addEventListener('click', (e) => this.handleOutsideClick(e));
+
         document.querySelectorAll(`#${this.idSelect} .c-select__dropdown-item`)
-            .forEach(item => item.addEventListener("keydown", (e) => this.handleKeyboard(e, this.idSelect, config)));
+            .forEach(item => item.addEventListener("keydown", (e) => this.handleKeyboard(e)));
 
         document.querySelectorAll(`.c-select__dropdown-item`).forEach(item => {
             item.addEventListener('focus', (e) => e.target.focus());
         });
     }
 
-    handleKeyboard(e, idSelect, config) {
-        if (!e.target.classList.contains('c-select__dropdown-item')) return;
-        const SPACEBAR_KEY_CODE = 32;
-        const ENTER_KEY_CODE = 13;
-        const UP_KEY_CODE = 38;
-        const DOWN_KEY_CODE = 40;
-        const dropdownItems = document.querySelectorAll(`#${idSelect} .c-select__dropdown-item`);
-        
-        switch (e.keyCode) {
-            case ENTER_KEY_CODE :
-                this.handleClick(e, idSelect, config);
-                break;
-            case SPACEBAR_KEY_CODE :
-                this.handleClick(e, idSelect, config);
-                break;
-            case UP_KEY_CODE :
-                dropdownItems.forEach((item, index) => {
-                    if (item === e.target) {
-                        item.dispatchEvent(new Event('blur'));
-                        index > 1 ? dropdownItems[index - 1].dispatchEvent(new Event('focus'))
-                        : dropdownItems[dropdownItems.length - 1]
-                            .dispatchEvent(new Event('focus'));                       
-                    }
-                });
-                break;
-            case DOWN_KEY_CODE :
-                dropdownItems.forEach((item, index) => {
-                    if (item === e.target) {
-                        item.dispatchEvent(new Event('blur'));
-                        index < dropdownItems.length - 1 ? dropdownItems[index + 1].dispatchEvent(new Event('focus'))
-                        : dropdownItems[1].dispatchEvent(new Event('focus'));   
-                    }
-                });
-            break;                       
-        }
-    }
+    handleKeyboard(e) {
+        if (!e.target.classList.contains('c-select__dropdown-item')) return;      
 
-    handleOutsideClick(e, idSelect) {
-        const selectedDiv = document.querySelector(`#${idSelect} .c-select__selected`);
-        const inputDiv = document.querySelector(`#${idSelect} .c-select__input-div`);
-        const arrowIcon = document.querySelector(`#${idSelect} .c-select__arrow-icon`);
-        const dropdown = document.querySelector(`#${idSelect} .c-select__dropdown`);
+        e.keyCode === this.ENTER_KEY_CODE || e.keyCode === this.SPACEBAR_KEY_CODE ? this.handleClick(e) : null;
 
-        if (selectedDiv.classList.contains('active')) {
-            if (this.isClickedOutside(e)) {
-                selectedDiv.classList.remove('active');
-                inputDiv.classList.remove('active');
-                arrowIcon.classList.remove('active');
-                dropdown.classList.remove('active');
-            }
+        if (e.keyCode === this.UP_KEY_CODE) {
+            this.dropdownItems.forEach((item, index) => {
+                if (item === e.target) {
+                    index > 1 ? this.dropdownItems[index - 1].dispatchEvent(new Event('focus'))
+                        : this.dropdownItems[this.dropdownItems.length - 1]
+                            .dispatchEvent(new Event('focus'));
+                }
+            });
         }
+
+        if (e.keyCode === this.DOWN_KEY_CODE) {
+            this.dropdownItems.forEach((item, index) => {
+                if (item === e.target) {
+                    index < this.dropdownItems.length - 1 ? this.dropdownItems[index + 1]
+                        .dispatchEvent(new Event('focus'))
+                        : this.dropdownItems[1].dispatchEvent(new Event('focus'));
+                }
+            });           
+        }        
     }
 
     isClickedOutside(e) {
@@ -115,23 +96,18 @@ export class Select {
         return this.isOutside;
     }
 
-    handleClick(e, idSelect, config) {
-        const input = document.querySelector(`#${idSelect} .c-select__input`);
-        const selectedDiv = document.querySelector(`#${idSelect} .c-select__selected`);
-        const inputDiv = document.querySelector(`#${idSelect} .c-select__input-div`);
+    handleClick(e) {
         const activeSelects = document.querySelectorAll(`.c-select__option-list .active`);
-        const arrowIcon = document.querySelector(`#${idSelect} .c-select__arrow-icon`);
-        const dropdown = document.querySelector(`#${idSelect} .c-select__dropdown`);
 
         if (e.target.classList.contains('c-select__selected') ||
             e.target.classList.contains('c-select__arrow') ||
             e.target.classList.contains('c-select__arrow-icon')) {
-            input.value = '';
-            input.dispatchEvent(new Event('input'));
-            selectedDiv.classList.toggle('active');
-            inputDiv.classList.toggle('active');
-            arrowIcon.classList.toggle('active');
-            dropdown.classList.toggle('active');
+            this.input.value = '';
+            this.input.dispatchEvent(new Event('input'));
+            this.selectedDiv.classList.toggle('active');
+            this.inputDiv.classList.toggle('active');
+            this.arrowIcon.classList.toggle('active');
+            this.dropdown.classList.toggle('active');
 
             if (activeSelects.length >= 4) {
                 activeSelects.forEach(select => {
@@ -140,46 +116,70 @@ export class Select {
             }
         }
 
-        e.target.classList.contains('c-select__dropdown-item') && this.handleOptionItemsClick(e, this.idSelect, config);
-        e.target.classList.contains('c-select__selected-remove') && this.handleSelectedClick(e, this.selectOptions);
+        e.target.classList.contains('c-select__dropdown-item') && 
+        this.handleOptionItemsClick(e, this.selectedDiv);
+
+        e.target.classList.contains('c-select__selected-remove') || 
+        e.target.classList.contains('c-select__selected-single-remove') ? 
+            this.handleSelectedClick(e, this.selectOptions) : null;   
     }
 
-    handleOptionItemsClick(e, idSelect, config) {
-        const selectedDiv = document.querySelector(`#${idSelect} .c-select__selected`);
+    handleOutsideClick(e) {
+        const selectedDiv = document.querySelector(`#${this.idSelect} .c-select__selected`);
+        const inputDiv = document.querySelector(`#${this.idSelect} .c-select__input-div`);
+        const arrowIcon = document.querySelector(`#${this.idSelect} .c-select__arrow-icon`);
+
+        if (selectedDiv.classList.contains('active')) {
+            if (this.isClickedOutside(e)) {
+                selectedDiv.classList.remove('active');
+                inputDiv.classList.remove('active');
+                arrowIcon.classList.remove('active');
+                this.dropdown.classList.remove('active');
+            }
+        }
+    }
+
+    handleOptionItemsClick(e, selectedDiv) {
         if (e.target === this.firstOptionItem) return;
         const selectedArr = [];
         const selectedItems = selectedDiv.querySelectorAll('.c-select__selected-div');
         selectedItems.forEach(elem => selectedArr.push(elem.textContent));
         !selectedArr.includes(e.target.textContent) &&
-            this.addToSelected(e, this.selectOptions, config, selectedDiv, selectedItems);
+            this.addToSelected(e, this.selectOptions, selectedDiv, selectedItems);
         e.target.classList.add('c-select__dropdown-item_selected');
     }
 
-    addToSelected(e, selectOptions, config, selectedDiv, selectedItems) {
-        if (!this.multi && selectedItems.length > 0){
-            this.handleSingleSelect(e, selectedItems, selectOptions);
-        } 
-        [...selectOptions].find(option => option.value === e.target.textContent).selected = true;       
+    addToSelected(e, selectOptions, selectedDiv, selectedItems) {
+        if (!this.multi && selectedItems.length > 0) this.handleSingleSelect(selectedItems);
+
+        [...selectOptions].find(option => option.value === e.target.textContent).selected = true;
         const selectedItemDiv = document.createElement('div');
         const selectedItemP = document.createElement('p');
-        const removeItemBtn = document.createElement('button');
         selectedItemDiv.classList.add('c-select__selected-div');
         selectedItemP.classList.add('c-select__selected-p');
-        removeItemBtn.classList.add('c-select__selected-remove');
         selectedItemP.textContent = e.target.textContent;
         selectedItemDiv.appendChild(selectedItemP);
-        selectedItemDiv.appendChild(removeItemBtn);
+        const removeItemBtn = document.createElement('button');
+
+        if (this.multi) {
+            removeItemBtn.classList.add('c-select__selected-remove');
+            selectedItemDiv.appendChild(removeItemBtn);
+        } else {
+            removeItemBtn.classList.add('c-select__selected-single-remove');           
+            selectedItemDiv.appendChild(removeItemBtn);
+        }
+
         selectedDiv.appendChild(selectedItemDiv);
     }
 
-    handleSingleSelect(e, selectedItems) {
+    handleSingleSelect(selectedItems) {
         const selectedDropdownItem = document.querySelector('.c-select__dropdown-item_selected');
         selectedDropdownItem.classList.remove('c-select__dropdown-item_selected');
         selectedItems[0].remove();
     }
 
     handleSelectedClick(e, selectOptions) {
-        [...selectOptions].find(option =>{
+        [...selectOptions].find(option => {
             return option.value === e.target.previousSibling.textContent;
         }).selected = false;
         e.target.parentElement.remove();
@@ -194,7 +194,7 @@ export class Select {
             const dropdownItemList = document.querySelector(`#${this.idSelect} .c-select__dropdown`);
             optionItem.classList.add("c-select__dropdown-item");
             optionItem.textContent = this.options[i].value;
-            optionItem.tabIndex='0';
+            optionItem.tabIndex = '0';
             dropdownItemList.appendChild(optionItem);
         }
     }
@@ -202,16 +202,15 @@ export class Select {
     filterOptionItems() {
         const input = document.querySelector(`#${this.idSelect} .c-select__input`);
         input.addEventListener('input', (e) => {
-            const dropdownItems = document.querySelectorAll(`#${this.idSelect} .c-select__dropdown-item`);
 
-            dropdownItems.forEach((item, index) => {
+            this.dropdownItems.forEach((item, index) => {
                 !item.textContent.toLowerCase().includes(e.target.value.toLowerCase()) && index !== 0
                     ? item.classList.add('isHidden') :
                     item.classList.remove('isHidden');
             });
 
-            if ([...dropdownItems]
-                .filter(elem => elem.classList.contains('isHidden')).length === dropdownItems.length - 1) {
+            if ([...this.dropdownItems]
+                .filter(elem => elem.classList.contains('isHidden')).length === this.dropdownItems.length - 1) {
                 this.firstOptionItem.textContent = 'no results';
             } else this.firstOptionItem.textContent = '';
         });
