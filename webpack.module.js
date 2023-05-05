834cb0e23c1cd563e51964162934647243e2de1a
Aleksandr Tochilin <alex8079@gmail.com>
2023-05-05 20:26:28 +0300

    webpack config fix(js compression)

--- webpack.module.js
+++ webpack.module.js
@@ -3,47 +3,49 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  devtool: "source-map",
-  entry: {
-    main: path.resolve(__dirname, './src/select.js')
-  },
-  experiments: {
-    outputModule: true,
-  },
-  plugins: [
-	new CleanWebpackPlugin(),
-	new MiniCssExtractPlugin()
-  ],
-  output: {
-    path: path.resolve(__dirname, './release'),
-    filename: '[name].min.js',
-	library: {
-      type: "module",
+    mode: 'production',
+    devtool: "source-map",
+    entry: {
+        main: path.resolve(__dirname, './src/select.js')
+    },
+    experiments: {
+        outputModule: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ],
+    output: {
+        path: path.resolve(__dirname, './release'),
+        filename: '[name].min.js',
+        library: {
+            type: "module",
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ],
     }
-  },
-  module: {
-    rules: [
-		{
-		 test: /\.(scss|css)$/,
-		 use: [
-			MiniCssExtractPlugin.loader,
-			'css-loader',
-			'postcss-loader',
-			'sass-loader'
-		 ],
-		},
-		{
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-		},
-    ],
-  },
-  optimization: {
-    minimizer: [
-      new CssMinimizerPlugin(),
-    ],
-  }
 };
